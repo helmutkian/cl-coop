f09e3a0d5f924f0d59f9fcdf9de36f82d6b8ee1b
helmutkian <helmut.rohrbacher@gmail.com>
2013-04-12 10:59:16 -0700

    Generators can now take arguments on their resumption

--- generator.lisp
+++ generator.lisp
@@ -34,29 +34,29 @@
    Takes a closure formed within a dynamic CL-CONT:WITH-CALL/CC 
    environment whose only argument represents the calling 
    continuation to be yielded to and returns a GENERATOR object."
-  (let (state)
-    (setf state
-	  (lambda (k)
-	    (funcall function
-		     (lambda (result)
-		       (setf k
-			     (let/cc resume
-			       (setf state resume)
-			       (funcall k result)))))
+  (let (local-state return/resume)
+    (setf local-state
+	  (lambda (&rest args)
+	    (apply function return/resume args)
 	    *generator-exhausted*))
+    (setf return/resume
+	  (lambda (&rest args)
+	    (let/cc cc
+	      (let ((old-state local-state))
+		(setf local-state cc)
+		(apply old-state args)))))
     (make-instance 'generator
-		   :continuation (lambda ()
-				   (call/cc state)))))
+		   :continuation return/resume)))
 
 ;;; ***************************************************************
 ;;; ***************************************************************
 
 
-(defun next (the-generator)
+(defun next (the-generator &rest args)
   "Handles advancing the execution of the generator to the 
    next value to be yielded."
   (when (not (deadp the-generator))
-    (let ((yield-value (funcall (continuation the-generator))))
+    (let ((yield-value (apply (continuation the-generator) args)))
       (when (eql yield-value *generator-exhausted*)
 	(setf (status-of the-generator) t
 	      yield-value nil))
@@ -66,17 +66,17 @@
  ;;; ***************************************************************
 ;;; ***************************************************************
 
-(defmacro with-generator (&body body)
+(defmacro with-generator (lambda-list &body body)
   "Macro which wraps it's body within a dynamic 
    CL-CONT:WITH-CALL/CC environment closure and constructs a
    GENERATOR thereof. The macro provides a local macro YIELD which
    yields a given value to the calling environment."
   (let ((yield-sym (gensym)))
     `(make-generator
-       (lambda/cc (,yield-sym)
+       (lambda/cc (,yield-sym ,@lambda-list)
 	 (macrolet 
 	     ((yield (&rest args) `(funcall ,',yield-sym ,@args)))
-	   ,@body))))))
+	   ,@body)))))
 
 
 
