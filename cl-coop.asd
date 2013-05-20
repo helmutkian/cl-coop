849ee6cf10ea590129840be6cfaa8fa9233cda1a
helmutkian <helmut.rohrbacher@gmail.com>
2013-05-19 21:19:51 -0700

    coroutine.lisp is ASDF buildable now

--- cl-coop.asd
+++ cl-coop.asd
@@ -4,7 +4,5 @@
   ((:system "cl-cont")
    (:file "packages"
 	  :depends-on ("cl-cont"))
-   (:file "generator"
-	  :depends-on ("packages"))
-   (:file "coroutines"
-	  :depends-on ("packages" "generator"))))
+   (:file "coroutine"
+	  :depends-on ("packages"))))
--- packages.lisp
+++ packages.lisp
@@ -1,30 +1,10 @@
-(defpackage #:com.helmutkian.cl-coop.generator
-  (:nicknames #:cl-coop.generator))
-  (:use #:common-lisp
-	#:cl-cont)
-  (:export #:generator
-	   #:make-generator
-	   #:next
-	   #:deadp
-	   #:with-generator))
-
 (defpackage #:com.helmutkian.cl-coop.coroutine
   (:nicknames #:cl-coop.coroutine)
   (:use #:common-lisp
-	#:com.helmutkian.cl-tacit
-	#:com.helmutkian.cl-coop.generator)
+	#:cl-cont)
   (:export #:coroutine
 	   #:make-coroutine
-           #:with-coroutine
-	   #:start))
-	   
- 
-(defpackage #:com.helmutkian.cl-coop.generator.test
-  (:nicknames #:cl-coop.generator.test)
-  (:use #:common-lisp
-	#:com.helmutkian.cl-coop.generator
-	#:cl-cont
-	#:5am)
-  (:export #:test-make-generator
-	   #:test-with-generator)
-  (:documentation "Package of unit tests for CL-COOP.GENERATOR package."))
+	   #:next
+	   #:deadp
+	   #:with-coroutine))
+
